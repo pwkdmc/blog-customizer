22b0c32c6349775693e1de8d227b9c62378df4fa
pwkdmc <ididun@icloud.com>
2026-09-11 02:20:16 +0300

    clean

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,4 +1,4 @@
-const path = require('path'); //для того чтобы превратить отнсительный путь в абсолютный мы будем использовать пакет path
+const path = require('path');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
@@ -6,10 +6,9 @@ module.exports = {
 	devtool: 'eval-source-map',
 	devServer: {
 		historyApiFallback: true,
-		static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-		// compress: true, // это ускорит загрузку в режиме разработки
-		port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-		open: true, // сайт будет открываться сам при запуске npm run dev
+		static: path.resolve(__dirname, './dist'),
+		port: 8080,
+		open: true,
 		hot: true,
 	},
 	plugins: [new ReactRefreshWebpackPlugin()],
